297400819a545eba2217db1449d824376a345ec9
btzr-io <btzr.io@gmail.com>
2019-05-07 20:18:06 -0600

    setup webpack

---
+++ examples/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>Test example!</div>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path =  require('path');
+const express =  require('express');
+const webpack =  require('webpack');
+
+const buildDirectory = path.resolve(__dirname, 'build');
+const exampleDirectory = path.resolve(__dirname, 'examples');
+
+
+module.exports = {
+  entry: './examples/index.js',
+  output: {
+   path: exampleDirectory,
+   publicPath: '/',
+   filename: 'bundle.js'
+ },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: exampleDirectory,
+    publicPath: '/build',
+    port: 8080,
+    before(app, server) {
+      app.use('/build', express.static(buildDirectory))
+    }
+  }
+};
