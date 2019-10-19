52dcc076697d114da2088fe63b02ef060986e7d5
maydayo <wanwisa.silanan@gmail.com>
2019-10-20 01:22:01 +0700

    page arrangement mangamode

--- src/context.js
+++ src/context.js
@@ -17,6 +17,7 @@ const defaultState = {
 
   // Settings
   theme: 'Dark',
+  mangaMode: false,
   bookMode: false,
   fullscreen: false,
   showControls: false,
@@ -103,7 +104,7 @@ export class ReaderProvider extends Component {
   }
 
   getPage = index => {
-    const { pages, bookMode, totalPages } = this.state
+    const { pages, bookMode, mangaMode, totalPages } = this.state
     const page = pages[index]
     const nextIndex = index + 1
     const nextPageExists = nextIndex >= 0 || nextIndex < totalPages
@@ -111,8 +112,11 @@ export class ReaderProvider extends Component {
       nextPageExists && !(index === 0 || index === totalPages - 1)
 
     // Return two pages
-    if (this.state.bookMode && shouldRenderBookMode) {
+    if (bookMode && shouldRenderBookMode) {
       const nextPage = pages[nextIndex]
+      if (mangaMode) {
+        return [nextPage, page]
+      }
       return [page, nextPage]
     }
 
