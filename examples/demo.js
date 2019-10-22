e09c8cd54f048032712eb3509a82fdfc06e8cdc3
Ismael Martín Alabarce <info@ismaeld.com>
2019-10-22 20:36:33 +0200

    Fields can be defined as well as inferred now. Refactor and add some methods to make code more readable. Added upport for select in field component

--- examples/demo.js
+++ examples/demo.js
@@ -12,8 +12,8 @@ class Demo extends Component {
     height: '500px',
     options: {
       workerUrl: '/build/worker-bundle.js',
-      theme: 'Light',
       preview: 0,
+      theme: {type: 'select', value: 'Light', options: ['Dark', 'Light']},
       mangaMode: false,
       allowFullScreen: true,
       autoHideControls: false,
@@ -25,31 +25,45 @@ class Demo extends Component {
     super(props)
   }
 
-  handleChange = ({target}, parent) => {
-    const name = target.name
-    let value = target.value
-    if (target.type === 'checkbox') value = target.checked
-    else if (target.type === 'file') value = target.files[0]
-    else if (!isNaN(Number(value))) value = Number(value)
-    if (parent)
-      this.setState({[parent]: {...this.state[parent], [name]: value}})
-    else
-      this.setState({[name]: value})
+
+
+  handleChange = ({target}) => {
+    const {name, value} = parseEvent(target)
+    this.setState({[name]: value})
   }
 
-  parseOptions() {
+  handleOptionsChange = ({target}) => {
+    const {options} = this.state
+    let {name, value} = parseEvent(target)
+    if (isObject(options[name])) value = {...options[name], value}
+    this.setState({'options': {...options, [name]: value}})
+  }
+
+  optionsToFields() {
     const options = this.state.options
-    return Object.keys(options).map(key => {
-      const value = options[key]
-      const type = typeof value === 'boolean' ? "checkbox" : "text"
+    return Object.entries(options).map(([key, value]) => {
+      let type = 'text'
+      if (isBoolean(value)) type = 'boolean'
+      if (isObject(options[key]) && value.type) type = value.type
       return <Field 
         key={key}
         name={key}
         type={type}
         value={this.state.options[key]}
-        onChange={event => this.handleChange(event, 'options')}
+        onChange={this.handleOptionsChange}
+        {...isObject(options[key]) ? value : undefined}
       />
     })
+  }
+
+  optionsToProps() {
+    return {
+      ...this.state,
+      options: Object.entries(this.state.options).reduce((options, [key, value]) => {
+        options[key] = isObject(value) ? value.value : value
+        return options
+      }, {})
+    }
   }
 
   render() {
@@ -65,27 +79,42 @@ class Demo extends Component {
             <Field name="file" type="file" onChange={this.handleChange}/>
             <Field name="width" value={this.state.width} onChange={this.handleChange}/>
             <Field name="height" value={this.state.height} onChange={this.handleChange}/>
-            {this.parseOptions()}
+            {this.optionsToFields()}
           </div>
         </aside>
         <div className="instance">
-          <Villain {...this.state} />
+          <Villain {...this.optionsToProps()} />
         </div>
       </div>
     )
   }
 }
 
-function Field({name, type = 'text', value, onChange}) {
+function Field({name, type = 'text', value, options = [], onChange}) {
+  let input = <input {...{name, type, value, onChange}} />
+  if (type === 'checkbox') input = <input {...{name, onChange}} type="checkbox" checked={value} />
+  else if (type === 'select') input = (
+    <select {...{name, value, onChange}}>
+      {options.map(option => <option key={option}>{option}</option>)}
+    </select>
+  )
   return (
     <label className={clsx('field', type === 'checkbox' && 'checkbox')}>
       <span>{name}</span>
-      {type === 'checkbox'
-        ? <input {...{name, onChange}} type="checkbox" checked={value} />
-        : <input {...{name, type, value, onChange}} />
-      }
+      {input}
     </label>
   )
+}
+
+const isObject = (value) => value && typeof value === 'object' && value.constructor === Object
+const isBoolean = (value) => typeof value === 'boolean'
+const parseEvent = (target) => {
+  const name = target.name
+  let value = target.value
+  if (target.type === 'checkbox') value = target.checked
+  else if (target.type === 'file') value = target.files[0]
+  else if (!isNaN(Number(value))) value = Number(value)
+  return {name, value}
 }
 
 export default Demo
