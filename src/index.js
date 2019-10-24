c6b3b39ed5a79436b547487c186cfb48a66b18be
Abbas Camp <abbascoder51@gmail.com>
2019-10-24 22:34:38 +0100

    - Split main css styles into seperate css files

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import CanvasRender from '@/components/render'
 import { ReaderContext, ReaderProvider } from '@/context'
 
 // Css
-import './css/styles.css'
+import './css/base.css'
 
 const defaultOpts = {
   theme: 'Dark',
