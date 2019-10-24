a5fb575134d3c2fd96f9c0ac3ed3181f3f90c653
Kamil Szmajdzinski <kamil.szmajdzinski@gmail.com>
2019-10-24 13:03:39 +0200

    Add custom file input

--- examples/demo.js
+++ examples/demo.js
@@ -1,39 +1,36 @@
 import React, { Component } from 'react'
 import clsx from 'clsx'
 import Villain from '@/index.js'
-import {version} from '../package.json';
+import { version } from '../package.json'
 import './demo.css'
 
 class Demo extends Component {
-
   state = {
     file: '/build/testFile/Example-archive.zip',
     options: {
       preview: 0,
-      theme: {type: 'select', value: 'Light', options: ['Dark', 'Light']},
+      theme: { type: 'select', value: 'Light', options: ['Dark', 'Light'] },
       mangaMode: false,
       allowFullScreen: true,
       autoHideControls: false,
       allowGlobalShortcuts: false,
-    }
+    },
   }
 
   constructor(props) {
     super(props)
   }
 
-
-
-  handleChange = ({target}) => {
-    const {name, value} = parseEvent(target)
-    this.setState({[name]: value})
+  handleChange = ({ target }) => {
+    const { name, value } = parseEvent(target)
+    this.setState({ [name]: value })
   }
 
-  handleOptionsChange = ({target}) => {
-    const {options} = this.state
-    let {name, value} = parseEvent(target)
-    if (isObject(options[name])) value = {...options[name], value}
-    this.setState({'options': {...options, [name]: value}})
+  handleOptionsChange = ({ target }) => {
+    const { options } = this.state
+    let { name, value } = parseEvent(target)
+    if (isObject(options[name])) value = { ...options[name], value }
+    this.setState({ options: { ...options, [name]: value } })
   }
 
   optionsToFields() {
@@ -42,36 +39,38 @@ class Demo extends Component {
       let type = 'text'
       if (isBoolean(value)) type = 'boolean'
       if (isObject(options[key]) && value.type) type = value.type
-      return <Field
-        key={key}
-        name={key}
-        type={type}
-        value={this.state.options[key]}
-        onChange={this.handleOptionsChange}
-        {...isObject(options[key]) ? value : undefined}
-      />
+      return (
+        <Field
+          key={key}
+          name={key}
+          type={type}
+          value={this.state.options[key]}
+          onChange={this.handleOptionsChange}
+          {...(isObject(options[key]) ? value : undefined)}
+        />
+      )
     })
   }
 
   optionsToProps() {
     return Object.entries(this.state.options).reduce((options, [key, value]) => {
-        options[key] = isObject(value) ? value.value : value
-        return options
-      }, {})
+      options[key] = isObject(value) ? value.value : value
+      return options
+    }, {})
   }
 
   render() {
-    const options = {  workerUrl: '/build/worker-bundle.js', ...this.optionsToProps()}
+    const options = { workerUrl: '/build/worker-bundle.js', ...this.optionsToProps() }
     return (
       <div className="villain-demo">
         <aside>
-         <header>
+          <header>
             <img src="./logo.png" alt="logo" />
             <span>{`v ${version}`}</span>
           </header>
           <div className="form">
             <h3>Options</h3>
-            <Field name="file" type="file" onChange={this.handleChange}/>
+            <Field name="file" type="file" onChange={this.handleChange} />
             {this.optionsToFields()}
           </div>
         </aside>
@@ -83,14 +82,23 @@ class Demo extends Component {
   }
 }
 
-function Field({name, type = 'text', value, options = [], onChange}) {
-  let input = <input {...{name, type, value, onChange}} />
-  if (type === 'boolean') input = <input {...{name, onChange}} type="checkbox" checked={value} />
-  else if (type === 'select') input = (
-    <select {...{name, value, onChange}}>
-      {options.map(option => <option key={option}>{option}</option>)}
-    </select>
+function Field({ name, type = 'text', value, options = [], onChange }) {
+  let input = (
+    <input
+      {...{ name, type, value, onChange }}
+      className={clsx(type === 'file' && 'custom-file-input')}
+    />
   )
+  if (type === 'boolean')
+    input = <input {...{ name, onChange }} type="checkbox" checked={value} />
+  else if (type === 'select')
+    input = (
+      <select {...{ name, value, onChange }}>
+        {options.map(option => (
+          <option key={option}>{option}</option>
+        ))}
+      </select>
+    )
   return (
     <label className={clsx('field', type === 'checkbox' && 'checkbox')}>
       <span>{name}</span>
@@ -99,17 +107,18 @@ function Field({name, type = 'text', value, options = [], onChange}) {
   )
 }
 
-const isObject = (value) => value && typeof value === 'object' && value.constructor === Object
+const isObject = value =>
+  value && typeof value === 'object' && value.constructor === Object
 
-const isBoolean = (value) => typeof value === 'boolean'
+const isBoolean = value => typeof value === 'boolean'
 
-const parseEvent = (target) => {
+const parseEvent = target => {
   const name = target.name
   let value = target.value
   if (target.type === 'checkbox') value = target.checked
   else if (target.type === 'file') value = target.files[0]
   else if (!isNaN(Number(value))) value = Number(value)
-  return {name, value}
+  return { name, value }
 }
 
 export default Demo
