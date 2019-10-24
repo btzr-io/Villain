80cee8722b630c032477191e0f4b13d36a92ac6c
edumoreira1506 <contato@eduardoem.com.br>
2019-10-24 14:40:55 -0300

    Changed select component to functional

--- src/components/toolbar/select.js
+++ src/components/toolbar/select.js
@@ -1,38 +1,25 @@
-import React, { Component } from 'react'
-import { ReaderContext } from '@/context'
+import React from 'react'
 import Icon from '@mdi/react'
 
-class WrapSelect extends Component {
-  static contextType = ReaderContext
-
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const { options, icon, value, onChange } = this.props
-    return (
-      <label>
-        <Icon
-          path={icon}
-          size={'22px'}
-          className={'villain-icon'}
-          style={{ marginTop: '7px', marginRight: '6px' }}
-        />
-        <select
-          value={value}
-          onChange={onChange}
-          style={{ position: 'relative', bottom: '6px' }}
-        >
-          {options.map(item => (
-            <option value={item} key={item}>
-              {item}
-            </option>
-          ))}
-        </select>
-      </label>
-    )
-  }
-}
+const WrapSelect = ({ options, icon, value, onChange }) =>
+  <label>
+    <Icon
+      path={icon}
+      size={'22px'}
+      className={'villain-icon'}
+      style={{ marginTop: '7px', marginRight: '6px' }}
+    />
+    <select
+      value={value}
+      onChange={onChange}
+      style={{ position: 'relative', bottom: '6px' }}
+    >
+      {options.map(item => (
+        <option value={item} key={item}>
+          {item}
+        </option>
+      ))}
+    </select>
+  </label>
 
 export default WrapSelect
