79e488713b2417b104b29728f66b8089e5227f02
Lucas Barros <englbaraujo@gmail.com>
2019-10-24 18:56:06 -0300

    Make Wrapp a functional component

--- src/components/wrapp.js
+++ src/components/wrapp.js
@@ -1,35 +1,27 @@
-import React, { Component } from 'react'
+import React, { useRef, useContext } from 'react'
 import { ReaderContext } from '@/context'
 import clsx from 'clsx'
 
-class Wrapp extends Component {
-  static contextType = ReaderContext
+const Wrapp = ({ children, width, height, preview }) => {
+  const context = useContext(ReaderContext)
+  const container = useRef(null)
+  const { theme, fullscreen, autoHideControls } = context.state
+  const size = { width, height }
 
-  constructor(props) {
-    super(props)
-    this.container = React.createRef()
-  }
-
-  render() {
-    const { width, height, preview } = this.props
-    const { theme, fullscreen, autoHideControls } = this.context.state
-    const size = { width, height }
-
-    return (
-      <div
-        ref={this.container}
-        className={clsx(
-          'villain',
-          theme,
-          fullscreen && 'villain-fullscreen',
-          !autoHideControls && 'villain--static'
-        )}
-        style={size}
-      >
-        {this.props.children(this.container ? this.container.current : null)}
-      </div>
-    )
-  }
+  return (
+    <div
+      ref={container}
+      className={clsx(
+        'villain',
+        theme,
+        fullscreen && 'villain-fullscreen',
+        !autoHideControls && 'villain--static'
+      )}
+      style={size}
+    >
+      {children(container ? container.current : null)}
+    </div>
+  )
 }
 
 export default Wrapp
