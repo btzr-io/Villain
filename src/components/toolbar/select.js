b0d149f2cc9b1c83898bd76402fba73e813a6334
Anil Kumar Pandiya <anilpandiya05@gmail.com>
2019-10-24 13:05:30 +0530

    changed class component to function component

--- src/components/toolbar/select.js
+++ src/components/toolbar/select.js
@@ -1,38 +1,28 @@
-import React, { Component } from 'react'
-import { ReaderContext } from '@/context'
 import Icon from '@mdi/react'
 
-class WrapSelect extends Component {
-  static contextType = ReaderContext
-
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const { options, icon, value, onChange } = this.props
-    return (
-      <label>
-        <Icon
-          path={icon}
-          size={'22px'}
-          className={'villain-icon'}
-          style={{ marginTop: '7px', marginRight: '6px' }}
-        />
-        <select
-          value={value}
-          onChange={onChange}
-          style={{ position: 'relative', bottom: '6px' }}
-        >
-          {options.map(item => (
-            <option value={item} key={item}>
-              {item}
-            </option>
-          ))}
-        </select>
-      </label>
-    )
-  }
-}
+const WrapSelect = (props) => {
+        const { options, icon, value, onChange } = props
+        return (
+            <label>
+                <Icon
+                    path={icon}
+                    size={'22px'}
+                    className={'villain-icon'}
+                    style={{ marginTop: '7px', marginRight: '6px' }}
+                />
+                <select
+                    value={value}
+                    onChange={onChange}
+                    style={{ position: 'relative', bottom: '6px' }}
+                >
+                    {options.map(item => (
+                        <option value={item} key={item}>
+                            {item}
+                        </option>
+                    ))}
+                </select>
+            </label>
+        )
+    }
 
 export default WrapSelect
