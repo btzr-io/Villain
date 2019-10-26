8352259ed29cde44ab7b147b667d15fc631eb5fa
Juan Riquelme González <me@juanriquel.me>
2019-10-26 22:33:00 +0200

    Add es locale messages

--- src/localize.js
+++ src/localize.js
@@ -1,7 +1,8 @@
 // ES6 module syntax
 import LocalizedStrings from 'react-localization'
-import EN from '@/locales/messages.json'
 import DE from '@/locales/messages.de.json'
+import EN from '@/locales/messages.json'
+import ES from '@/locales/messages.es.json'
 import FR from '@/locales/messages.fr.json'
 import PT from '@/locales/messages.pt.json'
 import RU from '@/locales/messages.ru.json'
@@ -9,9 +10,10 @@ import RU from '@/locales/messages.ru.json'
 const localize = new LocalizedStrings({
   EN,
   DE,
+  ES,
+  FR,
   PT,
   RU,
-  FR,
 })
 
 export default localize
