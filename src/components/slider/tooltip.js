76fb91e1d1473e58eec1b1e6ea18cc3220ee82a0
Thomas Luu <thomas.luu00@gmail.com>
2019-10-26 19:50:56 -0400

    Improved tooltip

--- src/components/slider/tooltip.js
+++ src/components/slider/tooltip.js
@@ -1,20 +1,11 @@
-import React, { Component, Fragment } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-class Tooltip extends Component {
-  static defaultProps = {
-    value: null,
-  }
-  render() {
-    const { value } = this.props
-
-    return (
-      <div className="tooltip">
-        <span className="tooltiptext">Value: {value}</span>
-      </div>
-    )
-  }
-}
+const Tooltip = ({ value = null }) => (
+  <div className="tooltip">
+    <span className="tooltiptext">Value: {value}</span>
+  </div>
+)
 
 Tooltip.propTypes = {
   value: PropTypes.number,
