8cbf6fcd3134f8a98888a3ab76d01555f2cc6d3a
joshwooding <12938082+joshwooding@users.noreply.github.com>
2019-10-27 14:45:45 +0000

    Convert toolbar to hooks

--- src/components/toolbar/index.js
+++ src/components/toolbar/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useContext, useState, useCallback } from 'react'
 import clsx from 'clsx'
 import Button from './button'
 import WrapSelect from './select'
@@ -19,195 +19,166 @@ import {
   mdiWeb,
 } from '@mdi/js'
 
-class Toolbar extends Component {
-  static contextType = ReaderContext
+const isFocused = () => {
+  const elem = document.activeElement
+  const contains = document.querySelector('div.villain').contains(elem)
+  const elemType = elem.tagName.toLowerCase()
+  const forbiddenElemTypes = ['input', 'textarea', 'button']
+  return contains && forbiddenElemTypes.indexOf(elemType) === -1
+}
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      lang: 'en-US',
-    }
-  }
+const Toolbar = ({
+  allowFullScreen,
+  showControls,
+  toggleFullscreen,
+  updateZoom,
+  renderError,
+  localize,
+}) => {
+  const {
+    state,
+    toggleSetting,
+    navigateForward,
+    navigateBackward,
+    navigateToPage,
+    toggleTheme,
+    togglePin,
+  } = useContext(ReaderContext)
 
-  isFocused = () => {
-    const elem = document.activeElement
-    const contains = document.querySelector('div.villain').contains(elem)
-    const elemType = elem.tagName.toLowerCase()
+  const {
+    theme,
+    pages,
+    bookMode,
+    mangaMode,
+    fullscreen,
+    currentPage,
+    currentZoom,
+    totalPages,
+    autoHideControls,
+    allowGlobalShortcuts,
+  } = state
 
-    if (
-      contains &&
-      elemType !== 'input' &&
-      elemType !== 'textarea' &&
-      elemType !== 'button'
-    ) {
-      return true
-    }
-    return false
-  }
+  const [lang, setLang] = useState('en-US')
+
+  useEffect(() => {
+    // Note: Unsure about this, it probalby affect peformance
+    Localize.setLanguage(lang)
+  }, [lang])
 
   // Note:? We should provide an api to add, define, overwrite key shortcuts
-  handleShortcuts = ({ key }) => {
-    const { toggleFullscreen } = this.props
-    const { allowGlobalShortcuts } = this.context.state
+  const handleShortcuts = useCallback(
+    event => {
+      // Check if it should restrict listening for key shortcuts on player focus
+      if (!isFocused() && !allowGlobalShortcuts) {
+        return
+      }
 
-    // Check if it should restrict listening for key shortcuts on player focus
-    if (!this.isFocused() && !allowGlobalShortcuts) {
-      return
+      const navigateRight = mangaMode ? navigateBackward : navigateForward
+      const navigateLeft = mangaMode ? navigateForward : navigateBackward
+
+      switch (event.key) {
+        // Toggle fullscreen of viewer.
+        // Note: Current conflict with openseadragon key shortcuts.
+        // Note: PreventDefault is used to remove flp shortcut.
+        case 'f':
+          event.preventDefault()
+          toggleFullscreen()
+          break
+
+        // Navigation to next page (previous when in mangaMode)
+        case 'ArrowRight':
+          navigateRight()
+          break
+
+        // Navigation to previous page (next when in mangaMode)
+        case 'ArrowLeft':
+          navigateLeft()
+          break
+      }
+    },
+    [allowGlobalShortcuts, mangaMode, navigateBackward, navigateForward, toggleFullscreen]
+  )
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleShortcuts)
+
+    return () => {
+      document.removeEventListener('keydown', handleShortcuts)
     }
+  }, [handleShortcuts])
 
-    const { toggleSetting, navigateForward, navigateBackward } = this.context
-    const { mangaMode } = this.context.state
-
-    const navigateRight = mangaMode ? navigateBackward : navigateForward
-    const navigateLeft = mangaMode ? navigateForward : navigateBackward
-
-    switch (key) {
-      // Toggle fullscreen of viewer.
-      // Note: Current conflict with openseadragon key shortcuts.
-      // Note: PreventDefault is used to remove flp shortcut.
-      case 'f':
-        event.preventDefault()
-        toggleFullscreen()
-        break
-
-      // Navigation to next page (previous when in mangaMode)
-      case 'ArrowRight':
-        navigateRight()
-        break
-
-      // Navigation to previous page (next when in mangaMode)
-      case 'ArrowLeft':
-        navigateLeft()
-        break
-    }
+  const handleLanguageChange = ({ target }) => {
+    setLang(target.value)
   }
 
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleShortcuts)
+  const layoutProps = {
+    icon: bookMode ? mdiBookOpen : mdiBookOpenOutline,
   }
 
-  // This don't work as expected:
-  // See: https://github.com/btzr-io/Villain/issues/107
-  /*
-  componentDidUpdate(prevProps, prevState) {
-    if(prevState.lang !== this.state.lang) {
-      Localize.setLanguage(this.state.lang)
-    }
-  }
-  */
+  const fullScreenIcon = fullscreen ? mdiFullscreenExit : mdiFullscreen
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleShortcuts)
+  const themeProps = {
+    icon: theme === 'Light' ? mdiWeatherNight : mdiWhiteBalanceSunny,
   }
 
-  handleLanguageChange = ({ target }) => {
-    this.setState({ lang: target.value })
-  }
+  const progress = (pages.length / totalPages) * 100
 
-  render() {
-    // Component Props
-    const {
-      allowFullScreen,
-      showControls,
-      toggleFullscreen,
-      renderError,
-      localize,
-    } = this.props
+  return (
+    <div className={clsx('villain-toolbar', !showControls && 'villain-toolbar-hide')}>
+      <NavigationControls currentPage={currentPage} totalPages={totalPages} />
 
-    // Actions
-    const {
-      state,
-      navigateForward,
-      navigateBackward,
-      toggleSetting,
-      toggleTheme,
-      togglePin,
-    } = this.context
+      <div className={'villain-toolbar-group villain-toolbar-group-expand'}>
+        <Slider
+          max={totalPages}
+          value={currentPage}
+          bufferProgress={progress}
+          onChange={navigateToPage}
+          reversed={mangaMode}
+        />
+      </div>
 
-    // State
-    const {
-      theme,
-      pages,
-      bookMode,
-      mangaMode,
-      fullscreen,
-      currentPage,
-      currentZoom,
-      totalPages,
-      autoHideControls,
-    } = state
-
-    const layoutProps = {
-      icon: bookMode ? mdiBookOpen : mdiBookOpenOutline,
-    }
-
-    const fullScreenIcon = fullscreen ? mdiFullscreenExit : mdiFullscreen
-
-    const themeProps = {
-      icon: theme === 'Light' ? mdiWeatherNight : mdiWhiteBalanceSunny,
-    }
-
-    const progress = (pages.length / totalPages) * 100
-
-    // Note: Unsure about this, it probalby affect peformance
-    Localize.setLanguage(this.state.lang)
-
-    return (
-      <div className={clsx('villain-toolbar', !showControls && 'villain-toolbar-hide')}>
-        <NavigationControls currentPage={currentPage} totalPages={totalPages} />
-
-        <div className={'villain-toolbar-group villain-toolbar-group-expand'}>
-          <Slider
-            max={totalPages}
-            value={currentPage}
-            bufferProgress={progress}
-            onChange={this.context.navigateToPage}
-            reversed={mangaMode}
-          />
-        </div>
-
-        <div className={'villain-toolbar-group'} disabled={renderError}>
-          <ZoomControls
-            onUpdate={this.props.updateZoom}
-            currentZoom={currentZoom}
-            disabled={renderError}
-          />
-          <div className="divider" />
+      <div className={'villain-toolbar-group'} disabled={renderError}>
+        <ZoomControls
+          onUpdate={updateZoom}
+          currentZoom={currentZoom}
+          disabled={renderError}
+        />
+        <div className="divider" />
+        <Button
+          type={'icon'}
+          icon={mdiPin}
+          active={!autoHideControls}
+          onClick={togglePin}
+          disabled={renderError}
+          tooltip={Localize['Pin controls']}
+        />
+        <Button
+          type={'icon'}
+          tooltip={bookMode ? Localize['Page view'] : Localize['Book view']}
+          onClick={() => toggleSetting('bookMode')}
+          disabled={renderError}
+          {...layoutProps}
+        />
+        <Button
+          type={'icon'}
+          tooltip={theme === 'Light' ? Localize['Dark theme'] : Localize['Light theme']}
+          onClick={toggleTheme}
+          disabled={renderError}
+          {...themeProps}
+        />
+        {allowFullScreen && (
           <Button
             type={'icon'}
-            icon={mdiPin}
-            active={!autoHideControls}
-            onClick={togglePin}
+            icon={fullScreenIcon}
+            tooltip={
+              fullscreen ? Localize['Exit fullscreen'] : Localize['Enter fullscreen']
+            }
+            tooltipClass={'right-edge'}
+            onClick={toggleFullscreen}
             disabled={renderError}
-            tooltip={Localize['Pin controls']}
           />
-          <Button
-            type={'icon'}
-            tooltip={bookMode ? Localize['Page view'] : Localize['Book view']}
-            onClick={() => toggleSetting('bookMode')}
-            disabled={renderError}
-            {...layoutProps}
-          />
-          <Button
-            type={'icon'}
-            tooltip={theme === 'Light' ? Localize['Dark theme'] : Localize['Light theme']}
-            onClick={toggleTheme}
-            disabled={renderError}
-            {...themeProps}
-          />
-          {allowFullScreen && (
-            <Button
-              type={'icon'}
-              icon={fullScreenIcon}
-              tooltip={
-                fullscreen ? Localize['Exit fullscreen'] : Localize['Enter fullscreen']
-              }
-              tooltipClass={'right-edge'}
-              onClick={toggleFullscreen}
-              disabled={renderError}
-            />
-          )}
-          {/*
+        )}
+        {/*
             Move this component to menu!
           <WrapSelect
             inputId="langSelector"
@@ -218,10 +189,9 @@ class Toolbar extends Component {
             label="Language toggle"
           />
          */}
-        </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Toolbar
