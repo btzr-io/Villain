ca2fc7543058d8840492b537b61cc4f6798724d2
Giuseppe Battistella <giuseppe.battistella@datarisk.io>
2019-10-30 17:40:51 -0300

    Fixes #196

--- src/components/toolbar/button.js
+++ src/components/toolbar/button.js
@@ -18,20 +18,22 @@ const ToolbarButton = ({
   ...otherProps
 }) => {
   const elem = (
-    <Button
-      title={title}
-      onClick={onClick}
-      disabled={disabled}
-      className={clsx('button', type && `button-${type}`, active && 'button--active')}
-      {...otherProps}
-    >
-      {icon && <Icon path={icon} size={'26px'} className={'villain-icon'} />}
-      {label && <span className={'villain-label'}>{label}</span>}
-      {children && <span className={'villain-label'}>{children}</span>}
-    </Button>
+    <span tabIndex={0} className={clsx('button-wrapper')}>
+      <Button
+        title={title}
+        onClick={onClick}
+        disabled={disabled}
+        className={clsx('button', type && `button-${type}`, active && 'button--active')}
+        {...otherProps}
+      >
+        {icon && <Icon path={icon} size={'26px'} className={'villain-icon'} />}
+        {label && <span className={'villain-label'}>{label}</span>}
+        {children && <span className={'villain-label'}>{children}</span>}
+      </Button>
+    </span>
   )
 
-  return !tooltip || disabled ? (
+  return !tooltip ? (
     elem
   ) : (
     <Tooltip title={tooltip} placement={tooltipPlacement}>
