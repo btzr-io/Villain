b72c09edcfc7f08f106b2fe2677cbe8d0cbff374
Larissagilliane <larissagilliane@hotmail.com>
2019-11-06 17:50:19 -0300

    Convert Villain to functional component

--- src/index.js
+++ src/index.js
@@ -1,65 +1,54 @@
-import React, { Component } from 'react'
+import React from 'react'
 import clsx from 'clsx'
 import PropTypes from 'prop-types'
 import Wrapp from '@/components/wrapp'
 import Uncompress from '@/components/uncompress'
 import CanvasRender from '@/components/render'
 import { ReaderContext, ReaderProvider } from '@/context'
-
 // Styles
 import '@/css'
 
-class Villain extends Component {
-  static contextType = ReaderContext
+const Villain = ({ file, width, height, options }) => {
+  const {
+    allowFullScreen,
+    allowGlobalShortcuts,
+    autoHideControls,
+    mangaMode,
+    preview,
+    theme,
+    workerUrl,
+  } = options
 
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const { file, width, height, options } = this.props
-
-    const {
-      allowFullScreen,
-      allowGlobalShortcuts,
-      autoHideControls,
-      mangaMode,
-      preview,
-      theme,
-      workerUrl,
-    } = options
-
-    return (
-      <ReaderProvider
-        defaultState={{ theme, autoHideControls, mangaMode, allowGlobalShortcuts }}
-      >
-        <Wrapp width={width} height={height} preview={preview}>
-          {container => (
-            <Uncompress file={file} workerUrl={workerUrl} preview={preview}>
-              <ReaderContext.Consumer>
-                {({ state }) => (
-                  <CanvasRender
-                    id={'osd-canvas-render'}
-                    hover={state.hover}
-                    focus={state.focus}
-                    theme={theme}
-                    container={container}
-                    bookMode={state.bookMode}
-                    mangaMode={mangaMode}
-                    allowGlobalShortcuts={allowGlobalShortcuts}
-                    currentPage={state.currentPage}
-                    allowFullScreen={allowFullScreen}
-                    autoHideControls={autoHideControls}
-                    renderError={state.renderError}
-                  />
-                )}
-              </ReaderContext.Consumer>
-            </Uncompress>
-          )}
-        </Wrapp>
-      </ReaderProvider>
-    )
-  }
+  return (
+    <ReaderProvider
+      defaultState={{ theme, autoHideControls, mangaMode, allowGlobalShortcuts }}
+    >
+      <Wrapp width={width} height={height} preview={preview}>
+        {container => (
+          <Uncompress file={file} workerUrl={workerUrl} preview={preview}>
+            <ReaderContext.Consumer>
+              {({ state }) => (
+                <CanvasRender
+                  id={'osd-canvas-render'}
+                  hover={state.hover}
+                  focus={state.focus}
+                  theme={theme}
+                  container={container}
+                  bookMode={state.bookMode}
+                  mangaMode={mangaMode}
+                  allowGlobalShortcuts={allowGlobalShortcuts}
+                  currentPage={state.currentPage}
+                  allowFullScreen={allowFullScreen}
+                  autoHideControls={autoHideControls}
+                  renderError={state.renderError}
+                />
+              )}
+            </ReaderContext.Consumer>
+          </Uncompress>
+        )}
+      </Wrapp>
+    </ReaderProvider>
+  )
 }
 
 Villain.propTypes = {
