b823f9a574c5da6a6feba75d9edb7c1c182750b9
btzr-io <btzr.io@gmail.com>
2019-12-01 13:10:00 -0700

    purge

--- packages/villain-react/dev-sandbox/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Demo from './demo.js'
-import './theme.css'
-import './demo.css'
-
-ReactDOM.render(<Demo />, document.getElementById('app'))
--- packages/villain-web/src/components/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function App({ context, children }) {
-  // NOTE: If you need to add or modify header, footer etc. of the app,
-  // please do that inside the Layout component.
-  return <React.Fragment>{React.cloneElement(children, context)}</React.Fragment>
-}
-
-export default React.memo(App)
