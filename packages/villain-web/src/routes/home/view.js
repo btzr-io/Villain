52dcce06b61ceca514de346d5eac5361ebaeb5e6
btzr-io <btzr.io@gmail.com>
2019-12-07 15:35:44 -0700

    update homepage
    add button component for navigation

--- packages/villain-web/src/routes/home/view.js
+++ packages/villain-web/src/routes/home/view.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import Footer from '@/components/footer'
 import Layout from '@/components/layout'
+import Button from '@/components/button'
+
 function View() {
   return (
     <Layout>
       <div className={'landing'}>
         <div className={'landing__content'}>
           <div className="landing__message">
-            <b>Select</b> comic book or <b>drop</b> it here.
+            <img
+              className="logo"
+              src="https://raw.githubusercontent.com/btzr-io/Villain/master/artworks/logo-small.png"
+            />
+          </div>
+
+          <div className="landing__message">
+            An open source web-based comic book reader.
           </div>
           <div className="landing__message">
-            <button className={'button'}>Try now</button>
-            <button className={'button button--secondary'}>Learn more</button>
+            <Button navigate="/reader" buttonType="primary">
+              Try it now!
+            </Button>
+            <Button navigate="/embed" buttonType="secondary">
+              Embed API
+            </Button>
           </div>
         </div>
       </div>
